6a03df298cd1f713d70dae772919f8e50deef965
GabeszLopusni <gabesz.lopusni@gmail.com>
2017-11-15 00:59:31 +0100

    last
    last

---
+++ kopaj/adminUsers.php
@@ -0,0 +1,15 @@
+<?php
+$result=mysqli_query($conn, "SELECT * FROM admin_users");
+
+?>
+<div id='adminok'>
+<table class="table">
+<?php
+	while($res= $result->fetch_assoc()){
+                        
+            }   
+
+?>
+
+</table>
+</div>
