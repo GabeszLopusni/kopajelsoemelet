c2f55926edeca24b14d99255117ff044c6be010e
Edvin Kőrösi <edwin1203@live.com>
2017-11-14 23:09:40 +0100

    login bootstrap

--- bosch/index.php
+++ bosch/index.php
@@ -6,9 +6,23 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="style.css">
     <title>Bejelentkezés</title>
+	<!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+<!-- Optional theme -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+
+<!-- Latest compiled and minified JavaScript -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+	<link href="//maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet">
+
 </head>
 <body>
-<form class="login" action="login.php" method="post">
+<div class="col-md-offset-8 col-md-3">
+<div class="container">
+<div class="row">
+
+<form class="form-login" action="login.php" method="post">
 <br>
 <label>Felhasználónév</label>
 <br>
@@ -17,12 +31,18 @@
 <br>
 <label>Jelszó</label>
 <br>
-
+<div class="wrapper">
+<span class="group-btn">
 <input type="password" name="password">
 <br>
 <br>
 <input type="submit" class="btn" value="Bejelentkezés">
+</span>
+</div>
 
+</div>
+</div>
+</div>
 </form>
 </body>
 </html>
