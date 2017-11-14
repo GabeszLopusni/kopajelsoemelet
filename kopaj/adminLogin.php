db0f82bafbdc8fc63937aa72a3cb1e45ed8479af
GabeszLopusni <gabesz.lopusni@gmail.com>
2017-11-15 00:48:40 +0100

    sadas
    asd

--- kopaj/adminLogin.php
+++ kopaj/adminLogin.php
@@ -6,36 +6,29 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="style.css">
     <title>Bejelentkezés</title>
-	<!-- Latest compiled and minified CSS -->
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
-
-<!-- Optional theme -->
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
-
-<!-- Latest compiled and minified JavaScript -->
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
-	<link href="//maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet">
-
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
 </head>
 <body>
-<div class="col-md-offset-8 col-md-3">
 <div class="container">
-<div class="row">
-
+    <div class="row">
+        <div class="col-md-offset-5 col-md-3">
 <form class="form-login" action="login.php" method="post">
 <br>
 <label>Felhasználónév</label>
 <br>
-<input type="text" name="username">
+<input class="form-control input-sm chat-input" type="text" name="username">
 <br>
 <br>
 <label>Jelszó</label>
 <br>
+
+<input type="password" class="form-control input-sm chat-input" name="password">
+<br>
+<br>
 <div class="wrapper">
 <span class="group-btn">
-<input type="password" name="password">
-<br>
-<br>
 <input type="submit" class="btn" value="Bejelentkezés">
 </span>
 </div>
--- kopaj/index.php
+++ kopaj/index.php
@@ -1,42 +1,80 @@
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">
+
+<?php
+session_start();
+ini_set("display_errors", 1);
+error_reporting(E_ERROR);
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname="kopaj_admin";
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+$results=mysqli_query($conn, "SELECT * FROM oldalak");
+
+?>
+
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="style.css">
-    <title>Bejelentkezés</title>
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+    <title>Teszt</title>
 </head>
 <body>
-<div class="container">
-    <div class="row">
-        <div class="col-md-offset-5 col-md-3">
-<form class="form-login" action="login.php" method="post">
-<br>
-<label>Felhasználónév</label>
-<br>
-<input class="form-control input-sm chat-input" type="text" name="username">
-<br>
-<br>
-<label>Jelszó</label>
-<br>
 
-<input type="password" class="form-control input-sm chat-input" name="password">
-<br>
-<br>
-<div class="wrapper">
-<span class="group-btn">
-<input type="submit" class="btn" value="Bejelentkezés">
-</span>
-</div>
+<nav class="navbar navbar-default">
+  <div class="container-fluid">
+    <div class="navbar-header">
+      <a class="navbar-brand" href="#">WebSiteName</a>
+    </div>
+    <ul class="nav navbar-nav">
+      <li class="active"><a href="#">Home</a></li>
+      <?php
+        
+        echo $_SESSION['tartalom'];        
+        while ($row = $results->fetch_assoc()){
+            
+        echo '<li><a href="?menu='.$row['id'].'">'.utf8_encode($row['cim']) .'</a></li>';
+                
+        }
+        
+        ?>
+    </ul>
+  </div>
+</nav>
+    
+    <div class="container">
 
-</div>
-</div>
-</div>
-</form>
+        <?php
+        if(isset($_GET['menu'])){
+            $result=mysqli_query($conn, "SELECT * FROM oldalak");            
+
+            while($res= $result->fetch_assoc()){
+                if($_GET['menu'] == $res['id']){
+                   echo $res['tartalom'];
+                }          
+            }   
+        }
+
+        ?>        
+    </div>
+	
+<footer>
+
+<a href="adminLogin.php">Admin oldal</a>
+</footer>
+    
+
 </body>
 </html>
 
